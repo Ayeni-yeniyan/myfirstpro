e7ed5c028c0c679dbe3859ce9b9218f3780c41ee
Ayeni-yeniyan <connectwithayeni@gmail.com>
2023-10-18 23:15:23 +0100

    final touch

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Text(
-                'FLUTTER INTERN',
+                'FLUTTER DEVELOPMENT INTERN',
                 style: TextStyle(
                   fontFamily: 'SourceSan3',
                   fontSize: 20,

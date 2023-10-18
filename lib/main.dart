3b2ea8994526d8849fac7cadb7c69a65d094c168
Ayeni-yeniyan <connectwithayeni@gmail.com>
2023-10-18 16:52:17 +0100

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,109 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 26, 43, 51),
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 5, 32, 26),
-          leading: const Icon(Icons.arrow_back_ios),
-          title: const Text(
-            'You are Poor!',
-          ),
-        ),
-        body: const Center(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image(image: AssetImage('assets/images/clw.jpg')),
-              Image(image: AssetImage('assets/images/clown.jpg')),
-              SizedBox(
-                height: 30,
+              Container(
+                width: double.infinity,
               ),
-              FloatingActionButton(
-                onPressed: null,
-                child: Icon(Icons.add),
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(
+                  'assets/images/Sam.jpg',
+                ),
+                //child: Image.asset(),
               ),
+              Container(
+                child: Text(
+                  'Ayeni Samuel',
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(5),
+                width: 100,
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(5),
+                width: 370,
+                child: Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Icon(Icons.phone, size: 20),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        child: Text(
+                          '+234 703 515 4083',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(5),
+                width: 370,
+                child: Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Icon(Icons.mail, size: 20),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        child: Text(
+                          'connectwithayeni@gmail.com',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
